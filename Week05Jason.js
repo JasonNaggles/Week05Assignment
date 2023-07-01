2258bf87567871a2d221dd48cad91e92ffa082b4
JasonNaggles <JasonNaggles.verizon.net>
2023-07-01 19:25:30 -0400

    first commit

--- Week05Jason.js
+++ Week05Jason.js
@@ -7,3 +7,20 @@ class Customer {
         return `${this.name} has this ${this.phoneNumber}.`;
     }
 }
+
+class Food {
+    constructor(name) {
+        this.name = name;
+        this.customers = [];
+    }
+    addFood(customer) {
+        if (customer instanceof Customer) {
+            this.customers.push(customer);
+        } else {
+            throw new Error(`You can only add an instance of Customer. Argument is not a customer: ${customer}`);
+        }
+    }
+
+}
+
+
