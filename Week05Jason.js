7fbccae5b5ef050419a62472e37d0b1ce954f34c
JasonNaggles <JasonNaggles.verizon.net>
2023-07-06 18:44:15 -0400

    first commit

--- Week05Jason.js
+++ Week05Jason.js
@@ -1,123 +1,150 @@
+const readline = require('readline');
+
 class Customer {
-    constructor(name, phoneNumber) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-    }
-    describe() {
-        return `${this.name} has this ${this.phoneNumber}.`;
-    }
+  constructor(name, phoneNumber) {
+    this.name = name;
+    this.phoneNumber = phoneNumber;
+  }
+
+  describe() {
+    return `${this.name} has this ${this.phoneNumber}.`;
+  }
 }
 
 class Food {
-    constructor(name, _customers) {
-        this.name = name;
-        this.customers = [];
+  constructor(name) {
+    this.name = name;
+    this.customers = [];
+  }
+
+  addFood(customer) {
+    if (customer instanceof Customer) {
+      this.customers.push(customer);
+    } else {
+      throw new Error(`You can only add an instance of Customer. Argument is not a customer: ${customer}`);
     }
-    addFood(customer) {
-        if (customer instanceof Customer) {
-            this.customers.push(customer);
-        } else {
-            throw new Error(`You can only add an instance of Customer. Argument is not a customer: ${customer}`);
-        }
-    }
-    describe() {
-        return `${this.name} has these ${this.customers.length} customers.`;
-    }
+  }
+
+  describe() {
+    return `${this.name} has ${this.customers.length} customers.`;
+  }
 }
 
 class Menu {
-    constructor(_food, _selectedFood) {
-        this.food = [];
-        this.selectedFood = null;
-    }
+  constructor() {
+    this.food = [];
+    this.selectedFood = null;
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  }
 
-    start() {
-        let selection = this.showMeMenuOptions();
-        while (selection != 0) {
-        switch (selection) {
-            case '1':
-            this.createFood;
-            break;
-            case '2':
-            this.viewFood;
-            break;
-            case '3':
-            this.deleteFood;
-            break;
-            case '4':
-            this.displayFood;
-            break;
-            default:
-                selection = 0; 
-        }
-            selection = this.showMeMenuOptions();
-        }
-            alert('Come back to see us!');
-    }
-    showMeMenuOptions() {
-        return prompt(`
-        0) Exit
-        1) Select new food
-        2) View your food selection
-        3) Remove your food selection
-        4) Display all of your food selection
-        `);
-    }
-    showFoodMenuOptions(foodInfo) {
-        return prompt(`
-        1) White Rice
-        2) Plain Fried Rice
-        3) Shrimp Fried Rice
-        4) Pork Fried Rice
-        5) Beef Fried Rice
-        6) Kung Pao Chicken
-        7) General Tso Chicken
-        ${foodInfo}
-        `);
-    }
-    displayAllYourFoodSelection() {
-        let foodString = '';
-        for (let i = 0; i < this.food.length; i++) {
-            foodString += i + ') ' + this.food[i].name + '\n';
-        }
-        alert(foodString);
-    }
-    createFood() {
-        let name = prompt('Enter name for new food:');
-        this.food.push(new Food(name));
-    }
-    viewFood() {
-        let index = prompt('Enter the index of the food you wish to view:');
-        if (index > -1 && index < this.food.length) {
-            this.selectedFood = this.food[index];
-            let description = 'Food Name: ' + this.selectedFood.name + '\n';
+  start() {
+    this.showMeMenuOptions();
+  }
 
-            for (let i = 0; i < this.selectedFood.customers.length; i++) {
-                description += i + ') ' + this.selectedFood.customers[i].name + ' - ' + this.selectedFood.customers[i].phoneNumber + '\n';
-            }
-            let selection = this.showCustomersMenuOptions(description);
-            switch (selection) {
-                case '1':
-                this.createCustomer();
-                break;
-                case '2':
-                this.deleteCustomer();
-            }
+  showMeMenuOptions() {
+    this.rl.question(
+      `
+      0) Exit
+      1) Select new food
+      2) View your food selection
+      3) Remove your food selection
+      4) Display all of your food selection
+      `,
+      (selection) => {
+        switch (selection.trim()) {
+          case '0':
+            this.rl.close();
+            console.log('Come back to see us!');
+            break;
+          case '1':
+            this.createFood();
+            break;
+          case '2':
+            this.viewFood();
+            break;
+          case '3':
+            this.deleteFood();
+            break;
+          case '4':
+            this.displayAllYourFoodSelection();
+            break;
+          default:
+            this.showMeMenuOptions();
+            break;
         }
-    
-    }
-    createCustomer() {
-        let name = prompt('Enter name for new customer:');
-        let phoneNumber = prompt('Enter telephone number of new customer:');
-        this.selectedFood.customers.push(new Customer(name, phoneNumber));
+      }
+    );
+  }
 
+  showFoodMenuOptions(foodInfo) {
+    this.rl.question(
+      `
+      1) White Rice
+      2) Plain Fried Rice
+      3) Shrimp Fried Rice
+      4) Pork Fried Rice
+      5) Beef Fried Rice
+      6) Kung Pao Chicken
+      7) General Tso Chicken
+      ${foodInfo}
+      `,
+      (selection) => {
+        // Handle the user's food selection
+      }
+    );
+  }
+
+  displayAllYourFoodSelection() {
+    let foodString = '';
+    for (let i = 0; i < this.food.length; i++) {
+      foodString += i + ') ' + this.food[i].name + '\n';
     }
-    deleteCustomer() {
-        let index = prompt('Enter the index of the customer that you want to delete:');
-        if (index > -1 && index < this.selectedFood.customers.length) {
-            this.selectedFood.customers.splice(index, 1);
+    console.log(foodString);
+    this.showMeMenuOptions();
+  }
+
+  createFood() {
+    this.rl.question('Enter name for new food: ', (name) => {
+      this.food.push(new Food(name));
+      this.showMeMenuOptions();
+    });
+  }
+
+  viewFood() {
+    this.rl.question('Enter the index of the food you wish to view: ', (index) => {
+      index = parseInt(index);
+      if (index > -1 && index < this.food.length) {
+        this.selectedFood = this.food[index];
+        let description = 'Food Name: ' + this.selectedFood.name + '\n';
+
+        for (let i = 0; i < this.selectedFood.customers.length; i++) {
+          description += i + ') ' + this.selectedFood.customers[i].name + ' - ' + this.selectedFood.customers[i].phoneNumber + '\n';
         }
-    }
+
+        console.log(description);
+        // Continue with menu options for viewing food
+      } else {
+        console.log('Invalid index');
+        this.showMeMenuOptions();
+      }
+    });
+  }
+
+  deleteFood() {
+    this.rl.question('Enter the index of the food you want to delete: ', (index) => {
+      index = parseInt(index);
+      if (index > -1 && index < this.food.length) {
+        this.food.splice(index, 1);
+        this.showMeMenuOptions();
+      } else {
+        console.log('Invalid index');
+        this.showMeMenuOptions();
+      }
+    });
+  }
 }
 
 let menu = new Menu();
