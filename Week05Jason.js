0d3dd820f2c401d2a903eb73aa88def2448be323
JasonNaggles <JasonNaggles.verizon.net>
2023-07-08 20:51:29 -0400

    first commit

--- Week05Jason.js
+++ Week05Jason.js
@@ -10,35 +10,35 @@ class Food {
 class Menu {
     constructor() { 
         this.food = []; //food array
-        this.selectedFood = null; 
+        this.selectedFood = null; // work with one piece of food at a time
             }
             
             start() {
-            let selection = this.showMeMenu();
-            while (selection != 0) {
+            let selection = this.showMeMenu(); // initially, displays the menu using the showMeMenu method
+            while (selection != 0) { // as long as the user does not enter 0, then execute the option that was chosen
             switch (selection) {
                     case '1':
-                    this.createFood();
+                    this.createFood(); // if 1 is chosen, execute the createFood method in this class
                     break;
                     case '2':
-                    this.viewFood();
+                    this.viewFood(); // if 2 is chosen, execute the viewFood method in this class
                     break;
                     case '3':
-                    this.deleteFood();
+                    this.deleteFood(); // if 3 is chosen, exceute the deleteFood method in this class
                     break;
                     case '4':
-                    this.displayAllYourFoodSelection();
+                    this.displayAllYourFoodSelection(); // if 4 is chosen, execute the displayAllYourFoodSelection method in this class
                     break;
                     default:
-                        selection = 0; 
+                        selection = 0; // in a user does not chose an option from above, set the selection to 0 which will execute
                 }
-                    selection = this.showMeMenu();
+                    selection = this.showMeMenu(); //redisplay menu to user after selection
                 }
-                    alert('Come back to see us!');
+                    alert('Come back to see us!'); // when a user chooses 0, show them a 'Come back to see us' message
             }
 
         
-            showMeMenu() {
+            showMeMenu() { // This method will display the menu options to the user as display below
                 return prompt(`
                 0) Exit
                 1) Select new food
@@ -47,7 +47,7 @@ class Menu {
                 4) Display all of your food selection
                 `);
             }
-            showFoodMenu() {
+            showFoodMenu() { // This method will display the Food Menu options and the name of each food. Caution!! This method will work after the user types 1 or more out of the 7 choices of food.
                 return prompt(`
                 0) Go Back
                 1) White Rice
@@ -60,7 +60,7 @@ class Menu {
                 --------------------------
                 `);
             }
-            displayAllYourFoodSelection() {
+            displayAllYourFoodSelection() { // Create a string to hold your food items
                 let foodString = "";
                 for (let i = 0; i < this.food.length; i++) {
                     foodString += i + ') ' + this.food[i].foodName + '\n';
@@ -68,21 +68,22 @@ class Menu {
                 alert(foodString);
             }
             createFood() {
-                let foodName = prompt('Enter name for new food: ');
-                this.food.push(new Food(foodName));
+                let foodName = prompt('Enter name for new food: '); // Prompt the user for new food
+                this.food.push(new Food(foodName)); // adds a name of a food to end of the food array
             }
             viewFood() {
-                let index = prompt('Enter the index of the food you wish to view: '); 
-                  if (index > -1 && index < this.food.length) {
-                    this.selectedFood = this.food[index];
-                    let description = 'Food Name: ' + this.selectedFood.foodName + '\n';
-                    description += " " + this.selectedFood.describe() + "\n";
+                let index = prompt('Enter the index of the food you wish to view: '); // Prompt the user to enter the index of the food they want to view
+                  if (index > -1 && index < this.food.length) { // make sure the number entered is a valid selection
+                    this.selectedFood = this.food[index]; // the select food becomes what is in the array index the user selected
+                    let description = 'Food Name: ' + this.selectedFood.foodName + '\n'; // sets description of the selected food name followed by a line break
+                    description += " " + this.selectedFood.describe() + "\n"; // adds each food to the description and a line break afterwards
 
-                    for (let i = 0; i < this.food.length; i++) {
-                        description += i + ") " + this.food[i].describe();
+                    //build list of selected foods
+                    for (let i = 0; i < this.food.length; i++) { // loop for the length of the food array
+                        description += i + ") " + this.food[i].describe(); // continues to add to the description of the food array
                     }
         
-                    let selection = this.showFoodMenu(description);
+                    let selection = this.showFoodMenu(description); // the selection is the index from the food prompted
                     switch (selection) {
                         case "1":
                         break;
